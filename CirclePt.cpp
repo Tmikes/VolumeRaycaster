9870a63002a8e2664daa0fe50ae8789f9fbb1efe
Tmikes <yecoux@gmail.com>
2018-09-14 09:28:43 +0200

    circle

--- CirclePt.cpp
+++ CirclePt.cpp
@@ -1,10 +1,71 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include "CirclePt.h"
+//#include <algorithm>
+#include <execution>
 
 
 
-CirclePt::CirclePt()
+std::vector<float> CirclePt::vertices()
 {
+	return mVertices;
+}
 
+std::vector<float> CirclePt::texcoords()
+{
+	return mTexcoords;
+}
+
+std::vector<float> CirclePt::center()
+{
+	return mCenter;
+}
+
+std::vector<unsigned short> CirclePt::indices(unsigned int pOffset)
+{
+	std::vector<unsigned short> tmp(mIndices);
+	std::for_each(std::execution::par_unseq, tmp.begin(), tmp.end(), [pOffset](unsigned short& item)
+	{
+		item += pOffset;
+	});
+	return tmp;
+}
+
+CirclePt::CirclePt(float pX, float pY, float pR)
+{
+	int segments = 100; 
+	for (int n = 0; n <= segments; n++) {
+		float const t1 = 2 * M_PI * (float)n / (float)segments;
+		float const t2 = 2 * M_PI * (float)(n+1) / (float)segments;
+		
+		// vertices
+		mVertices.push_back(pX);
+		mVertices.push_back(pY);
+		mVertices.push_back(pX + sin(t1) * pR);
+		mVertices.push_back(pY + cos(t1) * pR);
+		mVertices.push_back(pX + sin(t2) * pR);
+		mVertices.push_back(pY + cos(t2) * pR);
+
+		// texcoords
+		mVertices.push_back(pX);
+		mTexcoords.push_back(pX + sin(t1) * pR);
+		mVertices.push_back(pX + sin(t2) * pR);
+
+		// indices
+		mIndices.push_back(n * 3 + 0);
+		mIndices.push_back(n * 3 + 1);
+		mIndices.push_back(n * 3 + 2);
+
+
+		//center
+
+		mCenter.push_back(pX);
+		mCenter.push_back(pY);
+		mCenter.push_back(pX);
+		mCenter.push_back(pY);
+		mCenter.push_back(pX);
+		mCenter.push_back(pY);
+	}
 }
 
 
--- CirclePt.h
+++ CirclePt.h
@@ -5,9 +5,14 @@ class CirclePt
 {
 	std::vector<float> mVertices;
 	std::vector<float> mTexcoords;
+	std::vector<unsigned short> mIndices;
 	std::vector<float> mCenter;
 public:
-	CirclePt();
+	std::vector<float> vertices();
+	std::vector<float> texcoords();
+	std::vector<float> center();
+	std::vector<unsigned short> indices(unsigned int pOffset = 0);
+	CirclePt(float pX, float pY, float pR);
 	~CirclePt();
 };
 
